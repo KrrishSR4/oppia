226cc9d9f667123d5ea405c2366d002eac8a34d1
Mohit Ruwatia <120438312+mon4our@users.noreply.github.com>
2025-08-22 10:34:20 +0530

    [GSoC 2025] M1 Demo Fixes - Fix part of #18305: Study guides demo fixes (#23101)
    * Made requested fixes
    
    * Added conditional rendering for practice button
    
    * Fixed practice section functionality
    
    * Made requested breadcrumb changes
    
    * Fixed tests
    
    * Fixed acceptance test and added dev screenshots
    
    * Fixed backend test and addressed comments
    
    * Fixed acceptance test
    
    * Updated screenshot
    
    * Updated prod desktop screenshot
    
    * Updated prod mobile screenshot
    
    * Updated prod mobile screenshot
    
    * Nits

--- core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
+++ core/tests/puppeteer-acceptance-tests/utilities/user/curriculum-admin.ts
@@ -276,6 +276,8 @@ const editWorkedExampleModalAnswerRte =
 const rteComponentSaveButton = '.e2e-test-close-rich-text-component-editor';
 const topicPreviewTab = '.e2e-test-topic-preview-tab';
 const expandWorkedExampleButton = '.e2e-test-expand-workedexample';
+const subtopicExpandHeaderSelector = '.e2e-test-show-subtopics-list';
+const practiceTabToggle = '.e2e-test-toggle-practice-tab';
 
 const openExplorationEditorNavigationMobile =
   '.oppia-exploration-editor-tabs-dropdown.show';
@@ -2425,14 +2427,16 @@ export class CurriculumAdmin extends BaseUser {
     );
     await this.saveTopicDraft(topicName);
 
-    await this.createSkillForTopic(skillName, topicName, true);
-    await this.createQuestionsForSkill(skillName, 3);
+    await this.createSkillForTopic(skillName, topicName);
+    await this.createQuestionsForSkill(skillName, 10);
     await this.assignSkillToSubtopicInTopicEditor(
       skillName,
       subtopicName,
       topicName
     );
     await this.addSkillToDiagnosticTest(skillName, topicName);
+    await this.togglePracticeTabCheckbox();
+    await this.saveTopicDraft(topicName);
 
     await this.createSubtopicWithStudyGuideForTopic(
       'Subtracting Numbers',
@@ -2452,6 +2456,37 @@ export class CurriculumAdmin extends BaseUser {
     );
 
     await this.publishDraftTopic(topicName);
+  }
+
+  /**
+   * Toggles the "Show practice tab to learners" in Topic Editor.
+   */
+  async togglePracticeTabCheckbox(): Promise<void> {
+    if (this.isViewportAtMobileWidth()) {
+      await this.expectElementToBeVisible(subtopicExpandHeaderSelector);
+      await this.clickOn(subtopicExpandHeaderSelector);
+    }
+    try {
+      await this.page.waitForSelector(practiceTabToggle);
+      const practiceTabToggleElement = await this.page.$(practiceTabToggle);
+      if (!practiceTabToggleElement) {
+        throw new Error('Practice tab toggle not found.');
+      }
+      await this.waitForElementToBeClickable(practiceTabToggleElement);
+      await practiceTabToggleElement.click();
+
+      await this.page.waitForFunction(
+        (selector: string) => {
+          const element = document.querySelector(selector);
+          return (element as HTMLInputElement).checked === true;
+        },
+        {},
+        practiceTabToggle
+      );
+    } catch (error) {
+      console.error(error.stack);
+      throw error;
+    }
   }
 
   /**
